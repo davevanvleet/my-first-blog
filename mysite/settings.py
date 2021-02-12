2c66d76feb68be3987a694aa3f212cb6309e056c
David Van Vleet <56968216+davevanvleet@users.noreply.github.com>
2021-02-11 20:55:06 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#o5swt-pu42&qhcpeewv01t9i#_)b!(b+o-1700-g*^!#ck3(d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'davevanvleet.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
